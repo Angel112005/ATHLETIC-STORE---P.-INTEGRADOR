ed77913dbb98d91e86851051032c059926cafe80
Angel GMC <233381@ids.upchiapas.edu.mx>
2024-07-24 12:11:39 -0600

    Cambios de Diseño Atomico

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -10,13 +10,26 @@
 // export default Button;
 
 
+// import React from 'react';
+
+// export default function Button({ type = 'button', onClick, text, className }) {
+//   return (
+//     <button type={type} onClick={onClick} className={`py-2 px-4 rounded ${className}`}>
+//       {text}
+//     </button>
+//   );
+// }
+
 import React from 'react';
 
-export default function Button({ type = 'button', onClick, text, className }) {
+function Button({ onClick, type = 'button', children, className }) {
   return (
-    <button type={type} onClick={onClick} className={`py-2 px-4 rounded ${className}`}>
-      {text}
+    <button onClick={onClick} type={type} className={`py-2 px-4 rounded ${className}`}>
+      {children}
     </button>
   );
 }
+
+export default Button;
+
 
